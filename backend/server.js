1587aff97dbd220ba2a4cad404f4a08cbcbb84d0
vbenavi0 <victorvill.0988@gmail.com>
2024-02-28 10:04:40 -0500

    edited server.js

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,14 @@
-let express = require('express');
+import OpenAI from "openai";
+import dotenv from 'dotenv';
+import express from 'express';
 
+dotenv.config();
 
-let app = express();
+const app = express();
 const port = 5000;
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
 
 app.get('/', (req, res) => {
   res.send('Successful response.');
 });
 
-
 app.listen(port, () => console.log('Example app is listening on port 5000.'));
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
