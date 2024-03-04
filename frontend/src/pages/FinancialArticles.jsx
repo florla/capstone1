8c386fa0c8d4fb9a096987fbdeeb095181b8ec7d
Jessica Contreras Ayala <hoshostars@hotmail.com>
2024-03-04 09:52:08 -0500

    articles api

---
+++ frontend/src/pages/FinancialArticles.jsx
@@ -0,0 +1,17 @@
+// Article components LANDING PAGE 
+// http://localhost:3000/financialarticles
+
+import React from 'react';
+import Articles from './Articles';
+
+const App = () => {
+    return (
+        <div>
+            <Articles />
+            <button className="btn btn-primary">Financial News</button>
+
+        </div>
+    );
+};
+
+export default App;
