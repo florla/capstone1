d1b231858d5645a0d2d16fd7c438ad62e3374ca6
florla <feliciamlbrownwork@gmail.com>
2024-03-07 09:39:54 -0500

    footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 // Define Footer component
 export default function Footer({ className }) {
@@ -8,12 +10,27 @@ export default function Footer({ className }) {
         <footer className={`page-footer teal gradient-nav-foot ${className}`}>
             <div className="">
                 <div className="row">
-                    <div className="col l6 s12">
+                    <div className="col l4 s12">
                         {/* Paragraph describing the app */}
-                        <p className="grey-text text-lighten-4">Embrace the power of our app and unlock the secrets of the universe, one quiz at a time. As I always say, 'Yesterday is history, tomorrow is a mystery, but today is a gift. That is why it is called the present.'</p>
+                        <p className="grey-text text-lighten-4">Manage your finances like a pro with our budget app and Ride the Wave to Wealth! Stay on top of your expenses, track your income, and achieve your financial goals with ease.</p>
+                        <p className="white-text header-5">Follow Us On</p>
+                        <div className="col s4">
+                            <a href="https://www.linkedin.com" target="_blank" title="LinkedIn">
+                                <FontAwesomeIcon icon={faLinkedin} className="white-text fa-3x" />
+                            </a>
+                        </div>
+                        <div className="col s4">
+                            <a href="https://www.facebook.com" target="_blank" title="Facebook">
+                                <FontAwesomeIcon icon={faFacebook} className="white-text fa-3x" />
+                            </a>
+                        </div>
+                        <div className="col s4">
+                            <a href="https://twitter.com" target="_blank" title="Twitter">
+                                <FontAwesomeIcon icon={faTwitter} className="white-text fa-3x" />
+                            </a>
+                        </div>
                     </div>
-                    <div className="col l3 s12"></div>
-                    <div className="col l3 s12">
+                    <div className="col l4 s12">
                         <p className="white-text header-5">Links</p>
                         <ul>
                             {/* Links to different pages */}
@@ -25,10 +42,17 @@ export default function Footer({ className }) {
                             <li><Link className="white-text" to="/chatbot">Chatbot</Link></li>
                         </ul>
                     </div>
-                    <div className="col l3 s12">
+                    <div className="col l4 s12">
+                        <p className="white-text header-5">Newsletter</p>
+                        <div className="input-field white" style={{borderRadius: '5px'}}>
+                            <input id="email" type="email" className="validate white-text" />
+                            <label htmlFor="email">Email</label>
+                        </div>
+                        <button className="btn waves-effect waves-light teal darken-3" type="submit" name="action">Signup</button>
                     </div>
                 </div>
             </div>
         </footer>
     );
 }
+
